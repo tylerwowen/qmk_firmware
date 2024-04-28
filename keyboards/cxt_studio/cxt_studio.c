6d6de50fdd31049d653be966be3e7fb9aa5cf063
Tyler Ouyang <tylercowen@me.com>
2024-04-27 22:22:48 -0700

    my customization

--- keyboards/cxt_studio/cxt_studio.c
+++ keyboards/cxt_studio/cxt_studio.c
@@ -11,44 +11,40 @@ bool encoder_update_kb(uint8_t index, bool clockwise) {
         return false;
     }
     switch (index) {
-      case 0: {
-        if (clockwise) {
-            tap_code_delay(KC_VOLU, 10);
-        } else {
-            tap_code_delay(KC_VOLD, 10);
-        }
-      }
-      break;
-      case 1: {
-        if (clockwise) {
-            rgblight_increase_hue();
-        } else {
-            rgblight_decrease_hue();
-        }
-      }
-      break;
-      case 2: {
-        if (clockwise) {
-            rgblight_increase_val();
-        } else {
-            rgblight_decrease_val();
-        }
-      }
-      break;
-      case 3: {
-        if (clockwise) {
-            anim++;
-        } else {
-            anim--;
-        }
-	if (anim >= RGB_MATRIX_EFFECT_MAX) {
-		anim = 0;
-	} else if (anim < 0) {
-		anim = RGB_MATRIX_EFFECT_MAX - 1;
-	}
-	rgblight_mode(anim);
-      }
-      break;
+        case 0: {
+            if (clockwise) {
+                tap_code_delay(KC_VOLU, 10);
+            } else {
+                tap_code_delay(KC_VOLD, 10);
+            }
+        } break;
+        case 1: {
+            if (clockwise) {
+                rgblight_increase_hue();
+            } else {
+                rgblight_decrease_hue();
+            }
+        } break;
+        case 2: {
+            if (clockwise) {
+                rgblight_increase_val();
+            } else {
+                rgblight_decrease_val();
+            }
+        } break;
+        case 3: {
+            if (clockwise) {
+                anim++;
+            } else {
+                anim--;
+            }
+            if (anim >= RGB_MATRIX_EFFECT_MAX) {
+                anim = 0;
+            } else if (anim < 0) {
+                anim = RGB_MATRIX_EFFECT_MAX - 1;
+            }
+            rgblight_mode(anim);
+        } break;
     }
     return true;
 }
